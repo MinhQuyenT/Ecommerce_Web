0bd62c10ab6bf9a38f46cf9e84c6052ad2e42888
MinhQuyen <tmquyenit@gmail.com>
2024-06-12 16:20:35 +0700

    Header Nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Logo from "../../assets/logo.png"
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
 
 const Navbar = () => {
-  return <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white">
+  return <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
     {/* Upper Nav */}
     <div className="bg-primary/40 py-2">
         <div className="container flex justify-between items-center">
@@ -13,14 +16,28 @@ const Navbar = () => {
                 </a>
             </div>
             {/* Search bar */}
-            <div>
-                <div className="group">
+            <div className="flex justify-between items-center gap-4">
+                <div className="group relative hidden sm:block">
                     <input type="text" placeholder="Search" className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all
                     duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1
                     focus:border-primary dark:border-gray-500 dark:bg-gray-800
                     " />
+                    <IoMdSearch className="text-gray-500 group:hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
                 </div>
+                {/* Order Button */}
+            <button onClick={()=> alert("Ordering not available yet")} className="bg-gradient-to-r from-primary to-secondary transition-all duration-200
+                text-white py-1 px-4 rounded-full flex items-center gap-3 group
+            ">
+                <span className="group-hover:block hidden transition-all duration-200">Order</span>
+                <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer"/>
+            </button>
+
+            {/* DarkMode Button */}
+            <div>
+                <DarkMode />
             </div>
+            </div>
+            
         </div>
     </div>
     {/* Lower Nav */}
