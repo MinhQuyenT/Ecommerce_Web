09db5171f39f92f2df33e0e4cd83b71c0a2d6c8a
MinhQuyen <tmquyenit@gmail.com>
2024-07-01 16:03:32 +0700

    adjust

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -56,13 +56,22 @@ const Hero = ({ handleOrderPopup }) => {
                 <div>
                     <div className='grid grid-cols-1 sm:grid-cols-2'>
                         <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
-                            <h1 className='text-5xl sm:text6xl lg:text-7xl font-bold'>
+                            <h1 data-aos="zoom-out"
+                                data-aos-duration="500"
+                                data-aos-once="true" 
+                                className='text-5xl sm:text6xl lg:text-7xl font-bold'>
                                 {data.title}
                             </h1>
-                            <p className='text-sm'>
+                            <p
+                             data-aos="fade-up"
+                             data-aos-duration="500"
+                             data-aos-delay="100"
+                            className='text-sm'>
                                 {data.description}
                             </p>
-                            <div>
+                            <div  data-aos="fade-up"
+                                data-aos-duration="500"
+                                data-aos-delay="300">
                                 <button   onClick={handleOrderPopup} className='bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>
                                     Order Now
                                 </button>
@@ -70,7 +79,7 @@ const Hero = ({ handleOrderPopup }) => {
                         </div>
                         {/* Image section */}
                         <div className='order-2 sm:order-1'>
-                            <div className='relative z-10'>
+                            <div className='relative z-10'  data-aos="zoom-in" data-aos-once="true">
                                 <img src={data.img} alt='' className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto'/>
                             </div>
                         </div>
